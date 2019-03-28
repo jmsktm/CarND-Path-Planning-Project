9001b8efe38ed53e4c548507adc09f86922561a1
James <james@icloudtech.com>
2019-03-28 00:24:24 -0700

    Added a simple planner

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,7 @@
 #include "telemetry.h"
 #include "waypoint.h"
 #include "map.h"
+#include "planners/simpleplanner.h"
 
 using nlohmann::json;
 using std::string;
@@ -24,54 +25,23 @@ int main() {
                uWS::OpCode opCode) {
     string str(data);
     Telemetry telemetry = Telemetry(str);
+    SimplePlanner planner = SimplePlanner(map, telemetry);
 
     if (telemetry._hasTelemetryData()) {
-      double x = telemetry.get_x();
-      double y = telemetry.get_y();
-      double s = telemetry.get_s();
-      double d = telemetry.get_d();
-      double yaw = telemetry.get_yaw();
-      double speed = telemetry.get_speed();
-      auto previous_path_x = telemetry.get_previous_path_x();
-      auto previous_path_y = telemetry.get_previous_path_y();
-      double end_path_s = telemetry.get_end_path_s();
-      double end_path_d = telemetry.get_end_path_d();
-      auto sensor_fusion = telemetry.get_sensor_fusion();
-
-      json msgJson;
-      vector<double> next_x_vals;
-      vector<double> next_y_vals;
-
-      /**
-       * TODO: define a path made up of (x,y) points that the car will visit
-       * sequentially every .02 seconds
-       */
-      double dist_inc = 0.4;
-      double next_d = 6;
-      for (int i = 0; i < 50; ++i) {
-        double next_s = s + (i+1) * dist_inc;
-        vector<double> xy = getXY(next_s, next_d, map.get_waypoints_s(), map.get_waypoints_x(), map.get_waypoints_y());
-        next_x_vals.push_back(xy[0]);
-        next_y_vals.push_back(xy[1]);
-      }
-      msgJson["next_x"] = next_x_vals;
-      msgJson["next_y"] = next_y_vals;
-
+      json msgJson = planner.getRoute();
       auto msg = "42[\"control\","+ msgJson.dump()+"]";
       ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
     } else {
-      // Manual driving
       std::string msg = "42[\"manual\",{}]";
       ws.send(msg.data(), msg.length(), uWS::OpCode::TEXT);
     }
-  }); // end h.onMessage
+  });
 
   h.onConnection([&h](uWS::WebSocket<uWS::SERVER> ws, uWS::HttpRequest req) {
     std::cout << "Connected!!!" << std::endl;
   });
 
-  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code,
-                         char *message, size_t length) {
+  h.onDisconnection([&h](uWS::WebSocket<uWS::SERVER> ws, int code, char *message, size_t length) {
     ws.close();
     std::cout << "Disconnected" << std::endl;
   });
