c57c75a2e2382734e3e1881eecaef70f7f750b66
James <james@icloudtech.com>
2019-03-29 00:11:15 -0700

    Generate 3 points for spline generation

--- test/telemetry_tests.cpp
+++ test/telemetry_tests.cpp
@@ -3,6 +3,10 @@
 #include "../src/json.hpp"
 #include "../src/telemetry.h"
 
+#include <tuple>
+
+using std::tuple;
+
 TEST_CASE("telemetry.cpp >> _hasEventMessageData()") {
     REQUIRE_FALSE(Telemetry("")._hasEventMessageData());
     REQUIRE_FALSE(Telemetry("test")._hasEventMessageData());
@@ -72,4 +76,146 @@ TEST_CASE("Get properties from the (valid) telemetry data") {
     REQUIRE(telemetry.get_end_path_s() == 7.0);
     REQUIRE(telemetry.get_end_path_d() == 8.0);
     REQUIRE(telemetry.get_sensor_fusion() == R"([[1, 0.1], [2, 0.2]])"_json);
+}
+
+TEST_CASE("Return last two points when only current point and yaw are known") {
+    json data = R"(
+        {
+            "x": 100.0,
+            "y": 200.0,
+            "yaw": 45.0
+        }
+    )"_json;
+    string data_string = "42[\"telemetry\"," + data.dump() + "]";
+    Telemetry telemetry = Telemetry(data_string);
+    REQUIRE(telemetry.get_x() == 100.0);
+    REQUIRE(telemetry.get_y() == 200.0);
+    REQUIRE(telemetry.get_yaw() == 45.0);
+
+    tuple<vector<double>, vector<double>> last_two_points = telemetry.derive_last_two_points_from_current_pos();
+    vector<double> ptsx = std::get<0>(last_two_points);
+    vector<double> ptsy = std::get<1>(last_two_points);
+
+    // Rounding off to account for unpredictably changing precision
+    #define roundz(x,d) ((floor(((x)*pow(10,d))+.5))/pow(10,d))
+
+    vector<double> actual_ptsx = { roundz(ptsx[0], 3), roundz(ptsx[1], 3)};
+    vector<double> actual_ptsy = { roundz(ptsy[0], 3), roundz(ptsy[1], 3)};
+
+    vector<double> expected_ptsx = { roundz(99.4746780112, 3), roundz(100.00, 3) };
+    vector<double> expected_ptsy = { roundz(199.149096475, 3), roundz(200.00, 3) };
+
+    REQUIRE(actual_ptsx == expected_ptsx);
+    REQUIRE(actual_ptsy == expected_ptsy);
+}
+
+TEST_CASE("Get last two points as a tuple from previous_path_x and previous_path_y") {
+    json data = R"(
+        {
+            "previous_path_x": [ 100.00, 200.00, 300.00 ],
+            "previous_path_y": [ 120.00, 220.00, 320.00 ]
+        }
+    )"_json;
+    string data_string = "42[\"telemetry\"," + data.dump() + "]";
+    Telemetry telemetry = Telemetry(data_string);
+
+    REQUIRE(telemetry.get_previous_path_x() == R"([ 100.00, 200.00, 300.00 ])"_json);
+    REQUIRE(telemetry.get_previous_path_y() == R"([ 120.00, 220.00, 320.00 ])"_json);
+
+    vector<double> expected_ptsx = { 200.00, 300.00 };
+    vector<double> expected_ptsy = { 220.00, 320.00 };
+
+    tuple<vector<double>, vector<double>> last_two_points = telemetry.get_last_two_points_from_previous_path();
+    vector<double> actual_ptsx = std::get<0>(last_two_points);
+    vector<double> actual_ptsy = std::get<1>(last_two_points);
+
+    REQUIRE(expected_ptsx == actual_ptsx);
+    REQUIRE(expected_ptsy == actual_ptsy);
+}
+
+TEST_CASE("Get the next 3 evenly spaced waypoints as tuples of x and y coordinates") {
+    // Rounding off to account for unpredictably changing precision
+    #define roundz(x,d) ((floor(((x)*pow(10,d))+.5))/pow(10,d))
+
+    json data = R"(
+        {
+            "s": 100.0
+        }
+    )"_json;
+    string data_string = "42[\"telemetry\"," + data.dump() + "]";
+    Telemetry telemetry = Telemetry(data_string);
+    REQUIRE(telemetry.get_s() == 100.0);
+
+    tuple<vector<double>, vector<double>> last_3_waypoints = telemetry.get_next_3_evenly_spaced_waypoints(30, 2);
+    vector<double> ptsx = std::get<0>(last_3_waypoints);
+    vector<double> ptsy = std::get<1>(last_3_waypoints);
+
+    vector<double> actual_ptsx = { roundz(ptsx[0], 3), roundz(ptsx[1], 3), roundz(ptsx[2], 3) };
+    vector<double> actual_ptsy = { roundz(ptsy[0], 3), roundz(ptsy[1], 3), roundz(ptsy[2], 3) };
+
+    // The below numbers were not calculated manually, but rather taken
+    // directly from the the function invocation, as it's based on the
+    // route on the map, and not easily calculable.
+    vector<double> expected_ptsx = { roundz(914.6791548971, 3), roundz(945.623244928, 3), roundz(976.4068813369, 3) };
+    vector<double> expected_ptsy = { roundz(1124.88, 3), roundz(1126.162, 3), roundz(1130.728, 3) };
+
+    REQUIRE(actual_ptsx == expected_ptsx);
+    REQUIRE(actual_ptsy == expected_ptsy);
+}
+
+TEST_CASE("Get the 5 points for spline generation when less than 2 previous path points exist") {
+    json data = R"(
+        {
+            "x": 100.0,
+            "y": 200.0,
+            "yaw": 45.0,
+            "s": 100.0,
+            "previous_path_x": [],
+            "previous_path_y": []
+        }
+    )"_json;
+    string data_string = "42[\"telemetry\"," + data.dump() + "]";
+    Telemetry telemetry = Telemetry(data_string);
+
+    double gap = 30.0;
+    double target_lane = 2;
+    tuple<vector<double>, vector<double>> five_points = telemetry.get_5_points_for_spline_generation(gap, target_lane);
+    vector<double> ptsx = std::get<0>(five_points);
+    vector<double> ptsy = std::get<1>(five_points);
+
+    vector<double> actual_ptsx = { roundz(ptsx[0], 3), roundz(ptsx[1], 3), roundz(ptsx[2], 3), roundz(ptsx[3], 3), roundz(ptsx[4], 3) };
+    vector<double> actual_ptsy = { roundz(ptsy[0], 3), roundz(ptsy[1], 3), roundz(ptsy[2], 3), roundz(ptsy[3], 3), roundz(ptsy[4], 3) };
+
+    vector<double> expected_ptsx = { roundz(99.4746780112, 3), roundz(100.00, 3), roundz(914.6791548971, 3), roundz(945.623244928, 3), roundz(976.4068813369, 3) };
+    vector<double> expected_ptsy = { roundz(199.149096475, 3), roundz(200.00, 3), roundz(1124.88, 3), roundz(1126.162, 3), roundz(1130.728, 3) };
+
+    REQUIRE(actual_ptsx == expected_ptsx);
+    REQUIRE(actual_ptsy == expected_ptsy);
+}
+
+TEST_CASE("Get the 5 points for spline generation when more than 2 previous path points exist") {
+    json data = R"(
+        {
+            "s": 100.0,
+            "previous_path_x": [ 100.00, 200.00, 300.00 ],
+            "previous_path_y": [ 120.00, 220.00, 320.00 ]
+        }
+    )"_json;
+    string data_string = "42[\"telemetry\"," + data.dump() + "]";
+    Telemetry telemetry = Telemetry(data_string);
+
+    double gap = 30.0;
+    double target_lane = 2;
+    tuple<vector<double>, vector<double>> five_points = telemetry.get_5_points_for_spline_generation(gap, target_lane);
+    vector<double> ptsx = std::get<0>(five_points);
+    vector<double> ptsy = std::get<1>(five_points);
+
+    vector<double> actual_ptsx = { roundz(ptsx[0], 3), roundz(ptsx[1], 3), roundz(ptsx[2], 3), roundz(ptsx[3], 3), roundz(ptsx[4], 3) };
+    vector<double> actual_ptsy = { roundz(ptsy[0], 3), roundz(ptsy[1], 3), roundz(ptsy[2], 3), roundz(ptsy[3], 3), roundz(ptsy[4], 3) };
+
+    vector<double> expected_ptsx = { roundz(200, 3), roundz(300.00, 3), roundz(914.6791548971, 3), roundz(945.623244928, 3), roundz(976.4068813369, 3) };
+    vector<double> expected_ptsy = { roundz(220, 3), roundz(320.00, 3), roundz(1124.88, 3), roundz(1126.162, 3), roundz(1130.728, 3) };
+
+    REQUIRE(actual_ptsx == expected_ptsx);
+    REQUIRE(actual_ptsy == expected_ptsy);
 }
